ff5babec1fae6e99ea62f987a39f2544fbc0fe54
Ricardo Lima <phpricardo@gmail.com>
2020-09-07 15:24:45 -0300

    versioning API routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
-  resources :users, param: :_username
-  post '/auth/login', to: 'authentication#login'
-  get '/*a', to: 'application#not_found'
+  
+  namespace :api do
+    namespace :v1, defaults: { format: :json } do
+
+      get 'home/search/:user', to: 'home#search_repositories'
+
+      resources :users, param: :_username
+      post '/auth/login', to: 'authentication#login'
+      get '/*a', to: 'application#not_found'
+
+    end  
+  end
 end
