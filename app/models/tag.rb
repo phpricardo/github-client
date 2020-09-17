dd686f5c46895678d990360ae6cadfee63269209
Ricardo Lima <phpricardo@gmail.com>
2020-09-17 09:17:30 -0300

    Scope para busca via tags

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,16 +2,10 @@ class Tag < ApplicationRecord
   has_many :repository_tags
   has_many :repositories, :through => :repository_tags
 
-  # include PgSearch::Model
-  # pg_search_scope :search_repositories_by_tag, against: [:name],
-  # associated_against: {
-  #   repository_tags: :repository_id
-  # }
-
-  scope :search_repositories_by_tag, -> (tag) {
-    select('repositories.*')
-    .joins(:repository_tags, :repositories)
-    .where('name ilike ?', "%#{tag}%")
+  include PgSearch::Model
+  pg_search_scope :search_repositories_by_tag, against: [:name],
+  associated_against: {
+    repository_tags: :repository_id
   }
-  
+
 end
